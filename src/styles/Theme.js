e56cadd114234d695b6b1a0ee678ac911a332a6b
Daniel Hernández <daniel.hernandez.zafra@gmail.com>
2023-06-17 01:22:17 +0200

    Fix error due to lint change

--- src/styles/Theme.js
+++ src/styles/Theme.js
@@ -1,9 +1,8 @@
 export const Theme = {
   colors: {
-    powderWhite: "#FFFDF9",
-    persianGreen: "#06B49A",
-    lightBlue: "#AFDBD2",
-    onyx: "#36313D"
-  }
+    powderWhite: '#FFFDF9',
+    persianGreen: '#06B49A',
+    lightBlue: '#AFDBD2',
+    onyx: '#36313D',
+  },
 };
-  
